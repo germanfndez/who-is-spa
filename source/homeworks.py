08683abfd3f69c850241b7e8d2b34963eb5b722d
German Fernandez <germanfernandez0306@gmail.com>
2021-05-13 18:06:33 -0300

    Added methods to homeworks.py

--- source/homeworks.py
+++ source/homeworks.py
@@ -1,8 +1,27 @@
+from typing import ValuesView
 from database import mySQL
 
 class Homeworks(): 
     def __init__(self, userId):
-        mySQL.cursor.execute("SELECT * FROM users_works WHERE id_user", (userId))
+        mySQL.cursor.execute("SELECT * FROM users_works WHERE id_user = %s", (userId,))
         result = mySQL.cursor.fetchall()
         self.homeworks = result
-            
+        
+    def deleteHomework(self, homeworkId):
+        mySQL.cursor.execute("DELETE FROM users_works WHERE id = %s", (homeworkId,))
+        mySQL.database.commit()
+
+    def modifyHomework(self, homeworkId, nameA = None, descriptionA = None, data_startA = None, data_endA = None):
+        toModify = {'name':nameA, 'description':descriptionA, 'data_start':data_startA, 'data_end':data_endA}
+        for key, value in toModify.items():
+            if value != None:
+                print(key)
+                print(value)
+                #TODO create a consult
+                #mySQL.cursor.execute(f"UPDATE users_works SET {key} = {value} WHERE id = {homeworkId}")
+                #mySQL.database.commit()
+
+
+
+homerworkTest = Homeworks(1)
+homerworkTest.modifyHomework(1, nameA="asd")
