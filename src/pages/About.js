8175057224fa91b65011ef13165beea29b3f7735
German Fernandez <germanfernandez0306@gmail.com>
2022-09-23 21:44:26 -0300

    fix: problems with mobile hover

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ export const About = async () => {
       <h1 class='general-title'>About</h1>
       <p class='general-description'>A single-page application (SPA) is a web application or website that interacts with the user by dynamically rewriting the current web page with new data from the web server, instead of the default method of a web browser loading entire new pages. The goal is faster transitions that make the website feel more like a native app. <a href='https://en.wikipedia.org/wiki/Single-page_application' target='blank' class='general-description'>Read more</a></p>
       
-      <img class='about-image' alt='single page application diagram' src='../../assets/images/what-is-a-single-page-application-min.jpg'>
+      <img class='about-image' alt='single page application diagram' src='https://cdn.discordapp.com/attachments/869374902275301377/1023031909133074462/what-is-a-single-page-application-min.jpg'>
     </section>
   `;
 };
